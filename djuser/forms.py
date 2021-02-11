9ef06f7168bdc69e731599e938aa0bb0a9f1383e
siheon <korimse@gmail.com>
2021-02-11 18:30:20 +0900

    user form

---
+++ djuser/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import DJuser
+from django.contrib.auth.hashers import check_password
+
+
+class LoginForm(forms.Form):
+    username = forms.CharField(
+        error_messages={'required': '아이디를 입력해주세요'}, max_length=32, label="사용자 이름")
+    password = forms.CharField(error_messages={
+                               'required': '비밀번호를 입력해주세요'}, widget=forms.PasswordInput, label="비밀번호")
+
+    def clean(self):
+        cleaned_data = super().clean()
+        username = cleaned_data.get('username')
+        password = cleaned_data.get('password')
+
+        if username and password:
+            try:
+                djuser = DJuser.objects.get(username=username)
+                if not check_password(password, djuser.password):
+                    self.add_error('password', '비밀번호가 틀렸습니다.')
+                else:
+                    self.user_id = djuser.id
+            except(DJuser.DoesNotExist):
+                self.add_error('username', '아이디가 존재하지 않습니다.')
--- djuser/views.py
+++ djuser/views.py
@@ -2,7 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .models import DJuser
 from django.contrib.auth.hashers import make_password, check_password
-
+from .forms import LoginForm
 # Create your views here.
 
 
@@ -14,29 +14,22 @@ def home(request):
     return HttpResponse("Home!")
 
 
+def logout(request):
+    if request.session.get("user"):
+        del(request.session['user'])
+    return redirect('/')
+
+
 def login(request):
-    if request.method == 'GET':
-        return render(request, "login.html")
-    elif request.method == 'POST':
-        username = request.POST.get('username', None)
-        password = request.POST.get('password', None)
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            request.session['user'] = form.user_id
 
-        res_data = {}
-        if not (username and password):
-            res_data['error'] = '모든 값을 입력해야합니다.'
-        else:
-            try:
-                djuser = DJuser.objects.get(username=username)
-            except(DJuser.DoesNotExist):
-                res_data['error'] = '아이디가 틀렸습니다.'
-                return render(request, "login.html", res_data)
-            if check_password(password, djuser.password):
-                request.session['user'] = djuser.id
-                return redirect('/')
-            else:
-                res_data['error'] = '비밀번호를 틀렸습니다.'
-
-        return render(request, "login.html", res_data)
+            return redirect('/')
+    else:
+        form = LoginForm()
+    return render(request, "login.html", {'form': form})
 
 
 def register(request):
